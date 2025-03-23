043d115f2f73b8c0361fcd7c1952500554aba63d
ehcfbi <97340265+ehcfbi@users.noreply.github.com>
2025-03-23 22:05:41 +0900

    Update script.js

--- parrot/script.js
+++ parrot/script.js
@@ -5,7 +5,7 @@ document.getElementById('submit-button').addEventListener('click', function () {
     const specificValue = "23856169"; // 設定した特定の値
 
     if (inputBox.value.trim() === specificValue) { // 値が一致する場合
-        displayContent.style.display = 'block'; // コンテンツを表示
+        alert("You will begin to feel more deeply about things you have never paid attention to before. Password is "rh5623w$r"")
     } else {
         alert("that's not good at all."); // 値が一致しない場合のエラーメッセージ
     }
